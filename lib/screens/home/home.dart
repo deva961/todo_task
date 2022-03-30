4e17c165b84bdd0f16b5e1effe79875b3c6bc9fc
Deva Reddy <rajeshreddy.allala1@gmail.com>
2022-03-30 19:16:16 +0530

    first commit

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          children: [
+            Container(
+              height: 45,
+              width: 45,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset("assets/images/avatar.jpg"),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('Home Screen'),
+      ),
+    );
+  }
+}
